a61a944b2537581adee7894e5fa55d8c2ece0dfd
Brad Beer <brad@mint>
2012-12-23 22:41:20 -0500

    Added clinch-freeimage macros.

--- clinch-freeimage/clinch-freeimage.lisp
+++ clinch-freeimage/clinch-freeimage.lisp
@@ -1,40 +1,42 @@
 (in-package #:clinch)
 
 
-(defmacro with-loaded-32bit-map ((path &key width height bitvar widthvar heightvar) &body body)
-  "Loads a bitmap into memory using FreeImage.
-     width: If you want a specific width, set this.
-     height: If you want a specific height, set this.
-     bitvar: The variable name for the mapped data pointer.
-     widthvar: The variable name of the width (if width is set then equal to width)
-     heightvar: The variable name of the height (if height is set then equal to height)
+(defun create-texture-from-file (path &key width height)
+  (freeimage:with-loaded-32bit-map (path
+   				    :width     width
+   				    :height    height
+   				    :bitvar    bits
+   				    :widthvar  w
+   				    :heightvar h)
+    (make-instance 'clinch:texture
+		   :bits   bits
+		   :width  w
+		   :height h
+		   :stride 4
+		   :count  (* w h)
+		   :qtype  :unsigned-char
+		   :target :pixel-unpack-buffer)))
+	      
 
-     body:    Your code."
-  (let ((dib (gensym))
-	(orig-w (if widthvar widthvar (gensym)))
-	(orig-h (if heightvar heightvar (gensym)))
-	(bits (if bitvar bitvar 'bits))
-	(user-w (gensym))
-	(user-h (gensym))
-	(new-dib (gensym)))
-    `(let* ((,user-w ,width)
-	    (,user-h ,height)
-	    (,dib (freeimage::get-32bit-dib ,path))
-	    (,orig-w (freeimage:freeimage-getwidth ,dib))
-	    (,orig-h (freeimage:freeimage-getheight ,dib))
-	    (,bits))
 
-       (if (or ,user-w ,user-h)
-	   (let* ((new-dib (freeimage:freeimage-rescale ,dib ,user-w ,user-h)))
-	     (freeimage:unload-dib ,dib)
-	     (setf ,dib ,new-dib)
-	     (setf ,orig-w ,user-w)
-	     (setf ,orig-h ,user-h)))
-       
-       (setf ,bits (freeimage::freeimage-getbits ,dib))
-       (freeimage:freeimage-flipvertical ,dib)
-       ,@body
 
-       (freeimage:unload-dib ,dib))))
+(defmethod load-texture-from-file ((this texture) path &key)
+  (freeimage:with-loaded-32bit-map (path
+   				    :width     (width this)
+   				    :height    (height this)
+   				    :bitvar    src
+   				    :widthvar  w
+   				    :heightvar h)
+    
+    (bind this)
+    (%gl:Buffer-Data (target this)
+		     (* (get-size this) (cffi:foreign-type-size (qtype this)))
+		     src
+		     (usage this))
+    (setf (loaded? this) t)))
 
+    
+
+      
+      
 
