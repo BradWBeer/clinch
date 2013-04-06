8551e20c550f0558a4cc74fd85555f110f4cc368
WarWeasle1 <WarWeasle1@gmail.com>
2013-04-06 13:07:20 -0400

    More changes toward qix based picking.

--- entity.lisp
+++ entity.lisp
@@ -33,10 +33,52 @@
   (format s "#<entity>"))
 
 (defmethod get-render-value ((this entity) name)
-  (second
-   (assoc name
-	  (clinch::render-values this))))
+  (or (second
+       (assoc name
+	      (clinch::render-values this)))
+      (loop for i in (clinch::render-values this)
+	   if (and (>= 3 (length i))
+		   (equal name (second i)))
+	   do (return (third i)))))
 
+(defmethod get-primitive ((this entity) name)
+  (let* ((buff      (get-render-value this name))
+	 (stride    (stride buff))
+	 (icount    (vertex-count (indexes this)))
+	 (itype     (qtype (indexes this)))
+	 (btype      (clinch:qtype buff)))
+
+    (clinch:with-mapped-buffer (iptr (indexes this) :read-only)
+      (clinch:with-mapped-buffer (bptr buff :read-only)
+	(print (/ icount 3))
+	(loop
+	   for i from 0 to (1- (/ icount 3))
+	   collect (loop for j from 0 to 2
+		      collect (loop
+				 with ret = (make-array stride :element-type 'single-float)
+				 for k from 0 to (1- stride)
+				 do (setf (elt ret k)
+					  (cffi:mem-aref bptr btype
+							 (+ k (* (cffi:mem-aref iptr itype (+ (* i stride) j)) stride))))
+				 finally (return ret))))))))
+				   
+(defmethod triangle-intersection? ((this entity) start dir &key (vertex-name :vertices))
+  (labels ((rec (primitives i distance u v index)
+	     (multiple-value-bind (new-distance new-u new-v)
+		 (ray-triangle-intersect? start dir (first (car primitives)) (second (car primitives)) (third (car primitives)))
+	       (when (and new-distance
+			  (or (null distance)
+			      (< new-distance distance)))
+		 (setf distance new-distance
+		       u new-u
+		       v new-v
+		       index i)))
+	     (if (cdr primitives)
+		 (rec (cdr primitives) (1+ i) distance u v index)
+		 (values distance u v index))))
+    (rec (get-primitive this vertex-name) 0 nil nil nil nil)))
+			
+		   
 (defmethod make-render-func ((this entity) &key)
   (setf (slot-value this 'func)
 	(eval `(lambda (&key parent-transform projection-transform)
