7cf80ed57e6ba95cfe00f9c52c1f5991997ea822
WarWeasle1 <WarWeasle1@gmail.com>
2013-05-09 22:36:04 -0400

    Changes for Tutorial 7

--- clinch-pango/clinch-pango.lisp
+++ clinch-pango/clinch-pango.lisp
@@ -4,9 +4,9 @@
 (in-package #:clinch)
 (defvar *layout* '*layout*)
 
-(defmacro print-text (text &key (width nil) (wrap :pango_wrap_word))
+(defmacro print-text (text &key (width nil) (wrap :pango_wrap_word) (alignment :PANGO_ALIGN_CENTER))
   "Print a block of text."
-  `(with-paragraph (:width ,width :wrap ,wrap)
+  `(with-paragraph (:width ,width :wrap ,wrap :alignment ,alignment)
      (cairo:save)
      (pango:pango_layout_set_markup ,*layout* (xmls:toxml
 					      ,text) -1)
@@ -17,7 +17,7 @@
      (cairo:rel-move-to 0 (nth-value 1 (pango:get-layout-size ,*layout*)))))
 
 
-(defmacro with-paragraph ((&key (layout *layout*)  (context 'cairo:*context*) width wrap) &body body)
+(defmacro with-paragraph ((&key (layout *layout*)  (context 'cairo:*context*) (alignment :PANGO_ALIGN_CENTER) width (wrap :pango_wrap_word)) &body body)
   "Create a paragraph of text"
   (let ((gwidth (gensym))
 	(gwrap (gensym)))
@@ -30,7 +30,8 @@
      (when (and ,gwidth ,gwrap)
        (pango:pango_layout_set_wrap ,layout ,gwrap)
        (pango:pango_layout_set_width ,layout ,gwidth))
-
+     
+     (pango:pango_layout_set_alignment ,layout ,alignment)
      (unwind-protect 
 	  (progn ,@body)
        (pango:g_object_unref ,layout)))))
