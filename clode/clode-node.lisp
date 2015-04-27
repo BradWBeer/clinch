a654336f3ab0156563c2d1f6786418f52482c4fe
BradWBeer <BradWBeer@gmail.com>
2015-04-26 21:57:58 -0400

    Fixed some issues found during testing.

--- clode/clode-node.lisp
+++ clode/clode-node.lisp
@@ -6,8 +6,34 @@
     :initarg :geometry
     :reader geometry)))
 
-(defmethod update :before ((this node) &key parent force)
-  )
-  ;;(setf (transform this) (ode:get-transform (body (geometry this)))))
+(defmethod changed? ((this clode-node))
+  "Has this node changed and not updated?"
+  t)
+
+
+(defmethod update :before ((this clode-node) &key parent force)
+  (with-slots ((geom geometry)) this
+    (when geom 
+      (setf (clinch:transform this) (ode:get-transform geom)))))
+
+(defmethod initialize-instance :after ((this clode-node) &key parent)
+
+  (with-slots ((geom geometry)) this
+    (when geom (ref geom))))
+
+(defmethod (setf geom)  ((new-geom physics-object) (this clode-node))
+  (when new-geom (ref new-geom))
+  
+  (with-slots ((geom geometry)) this
+    (when geom (unref geom))))
+
+(defmethod unload :after ((this clode-node) &key)
+
+  (with-slots ((geom geometry)) this
+    (when geom (unref geom))))
+
+
+
+
 
 
