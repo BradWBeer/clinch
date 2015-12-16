3e121cb0abda98a066e1ed55bb2aa2c0c98aecba
BradWBeer <BradWBeer@gmail.com>
2015-12-15 23:18:50 -0500

    fixed build_ode.sh

--- clode/build_ode.sh
+++ clode/build_ode.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-./configure --prefix=/usr --enable-double-precision --enable-shared=yes --with-cylinder-cylinder=yes --with-box-cylinder=yes --with-capsule-cylinder=yes --disable-demos
+
+./configure --prefix=/usr --enable-double-precision --enable-shared=yes --enable-libccd --with-box-cylinder=libccd --disable-demos
+
 make -j 
