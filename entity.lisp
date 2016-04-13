bfc2c06ee7d3e954bc7f65956fe94ea1def40427
WarWeasle <warweasle1@gmail.com>
2016-04-13 14:39:47 +0000

    for merge

--- entity.lisp
+++ entity.lisp
@@ -3,41 +3,6 @@
 
 (in-package #:clinch)
 
-(defclass vao ()
-  ((id
-    :reader id
-    :initform nil
-    :initarg :id)
-   (changed 
-    :reader changed?
-    :initform t)
-   (current-value 
-    :initform (make-hash-table :test 'equal))))
-
-(defmethod initalize-instance ((this vao))
-  (with-slots ((id id)) this
-    (unless id
-      (setf id (gl:gen-vertex-arrays 1)))))
-
-(defmethod unload ((this vao) &key)
-  "Release buffer resources."
-  (trivial-garbage:cancel-finalization this)
-  (remove-uncollected this)
-  (!
-   (when (slot-value this 'id)
-     (gl:delete-buffers (list (id this)))))
-  (setf (slot-value this 'id) nil))
-
-(defmethod bind ((this vao))
-  (with-slots ((id id)) this
-    (when id
-      (gl:bind-buffer :array-buffer id))))
-
-(defmethod unbind ((this vao))
-  (when (id this)
-    (gl:bind-buffer :array-buffer 0)))
-
-
 (defclass entity ()
   ((shader-program
     :initform nil
