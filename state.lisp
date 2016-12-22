ee285ed0e416a3bd654a6339d38d3d8bbcf04853
BradWBeer <BradWBeer@gmail.com>
2016-12-22 20:58:51 +0000

    Forgot file

---
+++ state.lisp
@@ -0,0 +1,26 @@
+;;;; state.lisp
+;;;; Please see the licence.txt for the CLinch
+
+(in-package #:clinch)
+
+(defparameter *render-states* (make-hash-table))
+
+  ;; (gl:clear-color 0.0 0.0 0.0 0.0)
+  ;; (gl:enable :cull-face :blend :depth-test :depth-clamp)
+  ;; (gl:blend-func :src-alpha :one-minus-src-alpha)
+  ;; (gl:cull-face :back)
+  ;; (gl:front-face :ccw)
+  ;; (gl:depth-mask :true)
+  ;; (gl:depth-func :less)
+  ;; (gl:depth-range 0.0 1.0))
+
+
+(defun get-or-set-up-enabled-state (name enable)
+  (multiple-value-bind (value found?) (gethash name *render-states*)
+    (if found? 
+	(unless (eql enable value)
+	  (!gl:enable 
+	
+
+(defun !enable (value)
+  
