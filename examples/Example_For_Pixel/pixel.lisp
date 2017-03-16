c44dd6bfef3a33b1c6f8df00d8430551c055cb1f
warweasle <warweasle1@gmail.com>
2017-03-16 19:54:10 -0400

    Rendering and movement with keyboard and controller.

--- examples/Example_For_Pixel/pixel.lisp
+++ examples/Example_For_Pixel/pixel.lisp
@@ -5,8 +5,39 @@
 
 (use-package :clinch)
 
+(defconstant +max-axis+ 32767)
+(defparameter +axis-dead-zone+ .40)
+(defparameter *controller-vectors* (make-hash-table))
+
 (defparameter *weasle* nil)
 (defparameter *weasle-node* nil)
+(defparameter *weasle-max-velocity* 3)
+
+(defparameter *key-watch-list*
+  '(:scancode-left
+    :scancode-right
+    :scancode-up
+    :scancode-down
+    :scancode-space))
+
+(defparameter *key-state* nil)
+
+(defun add-to-key-state (scancode)
+  (unless (member scancode *key-state*)
+    (push scancode *key-state*)))
+
+(defun remove-from-key-state (scancode)
+  (when (member scancode *key-state*)
+    (setf *key-state* (delete scancode *key-state*))))
+
+(defun key-down? (scancode)
+  (member scancode *key-state*))
+
+
+;; (defun add-key-to-watchlist (key)
+;;(print (sdl2:scancode keysym))  
+;;   (sdl2:scancode-symbol 79)
+
 
 ;; Next runs one time before the next on-idle.
 (clinch:defevent clinch:*next* ()
@@ -23,14 +54,35 @@
   (setf *weasle-node* (make-instance 'node))
   (setf *weasle*
 	(make-quad-for-image "/home/warweasle/work/lisp/weasle.png"
+			     :height 180
 			     :parent *weasle-node*))
 
   (add-child *root* *weasle-node*))
 
-
+(defun move-weasle ()
+  (let ((up (key-down? :scancode-up))
+	(down (key-down? :scancode-down))
+	(left (key-down? :scancode-left))
+	(right (key-down? :scancode-right)))
+    (if (or up down left right)
+	(translate *weasle-node*
+		   (v3:*s
+		    (v3:+ (calculate-joysticks)
+			  (v3:normalize 
+			   (v3:+ (if up    (v!  0  1 0) (v! 0 0 0))
+				 (if down  (v!  0 -1 0) (v! 0 0 0))
+				 (if left  (v! -1  0 0) (v! 0 0 0))
+				 (if right (v!  1  0 0) (v! 0 0 0)))))
+		    (float *weasle-max-velocity*)))
+	(translate *weasle-node*
+		   (v3:*s (calculate-joysticks) (float *weasle-max-velocity*))))))
+     
 ;; render
 
 (clinch:defevent clinch:*on-idle* ()
+
+  (move-weasle)
+  
 
   (gl:clear :color-buffer-bit :depth-buffer-bit)
   ;; (test-step (nth 0 *animations*) *ticks*)
@@ -39,6 +91,13 @@
   ;; (clinch:render *node* :projection *projection*)
   (gl:clear :color-buffer-bit :depth-buffer-bit)
   (clinch:render *root* :projection *ortho-projection*))
+
+(defevent *on-key-down* (win keysym state ts)
+  (add-to-key-state (sdl2:scancode keysym)))
+
+(defevent *on-key-up* (win keysym state ts)
+  (remove-from-key-state (sdl2:scancode keysym)))
+
 
 ;; on window resize
 (clinch:defevent clinch:*on-window-resized* (win width height ts)
@@ -55,6 +114,54 @@
   
   (!S *root* (+ 1 (/ y 10)) (+ 1 (/ y 10)) 1))
 
+(defun calculate-joysticks ()
+  (let ((sum (v! 0 0 0)))
+    (maphash (lambda (k v)
+	       (when v
+		 (setf sum
+		       (v3:+ sum v))))
+	     *controller-vectors*)
+    (if (> (v3:length-squared sum) 1)
+	(v3:normalize sum)
+	sum)))
+
+;; 2 is right-Y and 3 is right-X
+(defevent *on-controller-axis-move* (controller-id axis-id position timestamp)
+
+  (let ((pos (float (/ position +max-axis+))))
+    (if (> (abs pos) +axis-dead-zone+)
+	;;(format t "controller=~A axis=~A pos=~A~%" controller-id axis-id 
+	(setf (gethash (+ (* controller-id 100) axis-id) *controller-vectors*)
+	      (if (evenp axis-id)
+		  (v! pos 0 0)
+		  (v! 0 (- pos) 0)))
+	(setf (gethash (+ (* controller-id 100) axis-id) *controller-vectors*)
+	      nil))))
 
 
-(init :init-controllers nil)
+  ;; (when (zerop controller-id)
+  ;;   (let ((pos (float (/ position +max-axis+))))
+  ;;     (case axis-id
+
+  ;; 	(5 nil)
+  ;; 	(4 nil)
+  ;; 	(3 (setf (aref +joystick-1+ 1) (- pos)))
+  ;; 	(2 (setf (aref +joystick-1+ 0) pos))
+  ;; 	(1 (setf (aref +joystick-2+ 1) (- pos)))
+  ;; 	(0 (setf (aref +joystick-2+ 0) pos))
+  ;; 	(t (format t "axis: ~A ~A~%" axis-id pos))))))
+
+;; (defevent *on-controller-button-down* (controller-id button ts)
+;;   (when (= button 10)
+;;     (let ((anna (entity-value :anna :body))
+;; 	  (ring (entity-value :ring :body)))
+
+;;       (setf (squirl:body-position ring) (squirl:body-position anna))
+;;       (setf (squirl:body-velocity ring)
+;; 	    (+
+;; 	     (squirl:body-velocity (entity-value :anna :body))
+;; 	     (* 1500 (v->complex (v2:normalize (entity-value :anna :direction)))))))))
+
+
+
+(init)
