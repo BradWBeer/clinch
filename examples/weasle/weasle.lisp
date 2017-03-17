d59cd36d1e591e4f4c2261ade2e9c5c216a4f0f3
warweasle <warweasle1@gmail.com>
2017-03-16 20:52:15 -0400

    Changed the name.

--- examples/weasle/weasle.lisp
+++ examples/weasle/weasle.lisp
@@ -6,25 +6,21 @@
 
 (use-package :clinch)
 
-;; Every channel's volume starts out at maximum according to the manual
+;; Sound stuff
 (defparameter *current-volume* 128)
 (defparameter *sound-effect* nil)
 
+;; Joystick stuff
 (defconstant +max-axis+ 32767)
 (defparameter +axis-dead-zone+ .40)
 (defparameter *controller-vectors* (make-hash-table))
 
+;; Game stuff
 (defparameter *weasle* nil)
 (defparameter *weasle-node* nil)
 (defparameter *weasle-max-velocity* 300) ;; in pixels/second
 
-(defparameter *key-watch-list*
-  '(:scancode-left
-    :scancode-right
-    :scancode-up
-    :scancode-down
-    :scancode-space))
-
+;; Keyboard handling
 (defparameter *key-state* nil)
 
 (defun add-to-key-state (scancode)
@@ -38,38 +34,41 @@
 (defun key-down? (scancode)
   (member scancode *key-state*))
 
-
-;; (defun add-key-to-watchlist (key)
-;;(print (sdl2:scancode keysym))  
-;;   (sdl2:scancode-symbol 79)
-
-
+;; Like an init function
 ;; Next runs one time before the next on-idle.
 (clinch:defevent clinch:*next* ()
 
-    (sdl2-mixer:init :ogg)
-    (sdl2-mixer:open-audio 22050 :s16sys 1 1024)
-    (sdl2-mixer:allocate-channels 1)
-    (setf *sound-effect* (sdl2-mixer:load-wav (asdf:system-relative-pathname 'clinch "examples/weasle/sample.ogg")))
+  ;; Set up sdl2-mixer
+  (sdl2-mixer:init :ogg)
+  (sdl2-mixer:open-audio 22050 :s16sys 1 1024)
+  (sdl2-mixer:allocate-channels 1)
+  (setf *sound-effect* (sdl2-mixer:load-wav (asdf:system-relative-pathname 'clinch "examples/weasle/sample.ogg")))
 
   ;; Enable a few opengl features. 
-  ;; (gl:enable :blend :depth-test :line-smooth :point-smooth :texture-2d :cull-face)
+  (gl:enable :blend :depth-test :line-smooth :point-smooth :texture-2d :cull-face)
 
-  ;; ;; Set the blending mode. 
-  ;; (%gl:blend-func :src-alpha :one-minus-src-alpha)
-  ;; (gl:polygon-mode :front-and-back :fill)
+  ;; Set the blending mode. 
+  (%gl:blend-func :src-alpha :one-minus-src-alpha)
+  (gl:polygon-mode :front-and-back :fill)
 
+  ;; Only sets the color, doesn't clear anything
   (gl:clear-color .5 .5 .5 0)
 
+  ;; Create a node to position the weasle sprite.
   (setf *weasle-node* (make-instance 'node))
+
+  ;; Load an image, resize it, and connect it to its node.
   (setf *weasle*
 	(make-quad-for-image (namestring (asdf:system-relative-pathname 'clinch "examples/weasle/weasle.png"))
 			     :height 180
 			     :width 129
 			     :parent *weasle-node*))
 
+  ;; Add node to root node so it's rendered.
   (add-child *root* *weasle-node*))
 
+;; Logic which combines keyboard and joystick input...it's buggy.
+;; The (v3: functions are in rtg-math and deal with 3 value vectors.
 (defun move-weasle ()
   (let ((up (key-down? :scancode-up))
 	(down (key-down? :scancode-down))
@@ -88,42 +87,33 @@
 	(translate *weasle-node*
 		   (v3:*s (calculate-joysticks) (float (* *delta-ticks* *weasle-max-velocity* 1/1000))))))
      
-;; render
-
+;; Render loop
 (clinch:defevent clinch:*on-idle* ()
 
   (move-weasle)
-  
 
-  (gl:clear :color-buffer-bit :depth-buffer-bit)
-  ;; (test-step (nth 0 *animations*) *ticks*)
-  ;; (gl:clear :color-buffer-bit :depth-buffer-bit)
-  ;; ;;(clinch:render entity :projection *projection*)
-  ;; (clinch:render *node* :projection *projection*)
   (gl:clear :color-buffer-bit :depth-buffer-bit)
   (clinch:render *root* :projection *ortho-projection*))
 
+;; Key press handler
 (defevent *on-key-down* (win keysym state ts)
   (add-to-key-state (sdl2:scancode keysym)))
 
+;; Key up handler
 (defevent *on-key-up* (win keysym state ts)
   (remove-from-key-state (sdl2:scancode keysym)))
 
-
-;; on window resize
+;; Window resize handler
+;; I didn't want to change the projection so I do nothing...not default behavior.
 (clinch:defevent clinch:*on-window-resized* (win width height ts)
-  ;;(format t "Resized: ~A ~A~%" width height)
-  
-  ;; (setf *projection* (clinch::make-perspective-transform (clinch:degrees->radians 45)
-  ;; 							 (/ width height) .1 1000))
   )
-
 
 ;; zoom in and out
 (clinch:defevent clinch:*on-mouse-wheel-move* (win mouse x y ts)
-  (format t "win=~A mouse=~A x=~A y=~A ts=~A~%" win mouse x y ts)
-  
+
+  ;; !S == (scale *weasle-node* ....)
   (!S *weasle-node* (+ 1 (/ y 10)) (+ 1 (/ y 10)) 1))
+
 
 (defun calculate-joysticks ()
   (let ((sum (v! 0 0 0)))
@@ -136,12 +126,11 @@
 	(v3:normalize sum)
 	sum)))
 
-;; 2 is right-Y and 3 is right-X
+;; Controller axis move handler
+;; It seems even joysticks are horizontal so I just went with it. 
 (defevent *on-controller-axis-move* (controller-id axis-id position timestamp)
-
   (let ((pos (float (/ position +max-axis+))))
     (if (> (abs pos) +axis-dead-zone+)
-	;;(format t "controller=~A axis=~A pos=~A~%" controller-id axis-id 
 	(setf (gethash (+ (* controller-id 100) axis-id) *controller-vectors*)
 	      (if (evenp axis-id)
 		  (v! pos 0 0)
@@ -149,11 +138,9 @@
 	(setf (gethash (+ (* controller-id 100) axis-id) *controller-vectors*)
 	      nil))))
 
-
+;; Button click handler
 (defevent *on-controller-button-down* (controller-id button ts)
-  (sdl2-mixer:play-channel 0 *sound-effect* 0)
-  (format t "controller=~A button=~A ts=~A~%" controller-id button ts)
-  )
+  (sdl2-mixer:play-channel 0 *sound-effect* 0))
 
-
+;; Start clinch...
 (init)
